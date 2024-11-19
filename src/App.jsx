731338041eb518e7e5a9db7b3c980347670902cc
GsxDavid <jmanceraram@gmail.com>
2024-11-19 15:57:01 -0500

    feat: Añade sistema de rutas para administrador y usuario
    - Implementa rutas específicas para la sección de administración y página principal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Landing from './components/Landing'
+import AdminRoutes from './components/admin-routes'
 
 function App() {
 
   return (
     <>
-      <Landing />
+      <AdminRoutes />
+      <Routes>
+        <Route path='/*' element={<Landing />} />
+        <Route path='/admin/*' element={<AdminRoutes />} />
+      </Routes>
     </>
   )
 }
